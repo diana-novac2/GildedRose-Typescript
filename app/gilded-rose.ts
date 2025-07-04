9053d8b4d66429d9830d95e891cde6fe78753531
Diana Novac <Diana.Novac@softwire.com>
2025-07-04 15:02:14 +0300

    refactored logic for decreasing quality items

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -23,10 +23,20 @@ export class GildedRose {
                 continue;
             }
 
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1
+                    this.items[i].quality -= 1;
                 }
+
+                this.items[i].sellIn -= 1;
+
+                if (this.items[i].sellIn < 0) {
+                    if (this.items[i].quality > 0) {
+                        this.items[i].quality -= 1;
+                    }
+                }
+
+                continue;
             } else {
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1
@@ -44,16 +54,12 @@ export class GildedRose {
                     }
                 }
             }
-            this.items[i].sellIn = this.items[i].sellIn - 1;
+
+            this.items[i].sellIn -= 1;
+
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            this.items[i].quality = this.items[i].quality - 1
-                        }
-                    } else {
                         this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
                 } else {
                     if (this.items[i].quality < 50) {
                         this.items[i].quality = this.items[i].quality + 1
